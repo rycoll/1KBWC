59e4af1eef6d9dda12c17d306b44e59b4dd9d470
rycoll <colry349@gmail.com>
2019-01-05 14:15:23 +1300

    Add win conditions (100pts by default)

--- Assets/Scripts/GamePlayer.cs
+++ Assets/Scripts/GamePlayer.cs
@@ -11,6 +11,11 @@ public class GamePlayer {
         Points = 0;
         DrawPerTurn = 1;
         Colour = Random.ColorHSV();
+        // set default win condition
+        Q_PlayerPoints pointQuery = ScriptableObject.CreateInstance<Q_PlayerPoints>();
+        RunTimeValue playerPointQuery = new RunTimeValue(pointQuery, this);
+        RunTimeValue winThreshold = new RunTimeValue(100);
+        WinCondition = new Condition(playerPointQuery, winThreshold, ConditionOperator.AT_LEAST);
     }
 
     public string Name { get; set; }
@@ -18,6 +23,7 @@ public class GamePlayer {
     public int Points { get; set; }
     public int DrawPerTurn { get; set; }
     public Color Colour { get; set; }
+    public Condition WinCondition { get; set; }
 
     public bool AddToHand (Card card)
     {
