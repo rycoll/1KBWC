1792fdca5f2ff43e128c155ba9e73426eb57a0fd
rycoll <colry349@gmail.com>
2019-01-25 22:01:50 +1300

    Improvements to card painting

---
+++ Assets/Scripts/CardEditor/PaintColourButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaintColourButton : MonoBehaviour
+{
+    public static Painter painter;
+
+    public Color colour;
+
+    public void SetPainterColour() {
+        painter.paintColour = colour;
+    }
+
+    private void Start () {
+        GetComponent<Image>().color = colour;
+    }
+}
--- Assets/Scripts/CardEditor/Painter.cs
+++ Assets/Scripts/CardEditor/Painter.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
     private Image image;
     private RectTransform rect;
     public Color paintColour;
-    public int brushSize;
+    private int brushSize;
+    private bool fillMode;
 
     private Texture2D texBuffer;
 
     int width = 300;
     int height = 300;
 
-    private int prevX, prevY;
+    private int prevX, prevY = -1;
 
     private void Start () {
         rect = GetComponent<RectTransform>();
@@ -24,6 +25,19 @@ public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         
         texBuffer= new Texture2D(width, height);
         image.material.mainTexture = texBuffer;
+
+        brushSize = 1;
+        fillMode = false;
+
+        PaintColourButton.painter = this;
+    }
+
+    public void SetBrushSize(float n) {
+        brushSize = (int) n;
+    }
+
+    public void SetFillMode(bool b) {
+        fillMode = b;
     }
 
     private Vector2 GetMouseCoord(PointerEventData eventData) {
@@ -38,13 +52,70 @@ public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         }
     }
 
-    public void OnBeginDrag(PointerEventData eventData){
-        Vector2 localCursor = GetMouseCoord(eventData);
-        prevX = (int)localCursor.x;
-        prevY = (int)localCursor.y;
+    private struct Coord {
+        public int x;
+        public int y;
+        public Coord (int i, int j) {
+            x = i;
+            y = j;
+        }
     }
 
+    private void Fill (int x, int y) {
+        Color[] colours = texBuffer.GetPixels();
+        Color targetColour = colours[y * height + x];
+        List<Coord> queue = new List<Coord>();
+        bool[,] visited = new bool[width,height];
+        
+        queue.Add(new Coord(x, y));
+        while (queue.Count > 0) {
+            Coord curr = queue[0];
+            queue.RemoveAt(0);
+            if (visited[curr.x, curr.y]) {
+                continue;
+            }
+            visited[curr.x, curr.y] = true;
+            colours[curr.y * height + curr.x] = paintColour;
+            Coord[] neighbours = new Coord[]{
+                new Coord(curr.x+1, curr.y),
+                new Coord(curr.x-1, curr.y),
+                new Coord(curr.x, curr.y+1),
+                new Coord(curr.x, curr.y-1)
+            };
+            foreach (Coord coord in neighbours) {
+                if (coord.x >= 0 && coord.x < width && coord.y >= 0 && coord.y < height) {
+                    if (!(visited[coord.x, coord.y])) {
+                        Color coordColour = colours[coord.y * height + coord.x];
+                        if (coordColour == targetColour) {
+                            queue.Insert(0, coord);
+                        }
+                    } 
+                }
+            }
+        }
+
+        texBuffer.SetPixels(colours);
+        image.material.mainTexture = texBuffer;
+        texBuffer.Apply();
+    }
+
+    public void OnPointerClick(PointerEventData eventData){
+        Vector2 localCursor = GetMouseCoord(eventData);
+        int x = (int)localCursor.x;
+        int y = (int)localCursor.y;
+        if (fillMode) {
+            Fill(x, y);
+        } else {
+            Paint(x, y);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {}
+
     public void OnDrag(PointerEventData eventData){
+        if (fillMode) {
+            return;
+        }
         Vector2 localCursor;
         bool isHit = RectTransformUtility.ScreenPointToLocalPointInRectangle(
             rect, eventData.position, eventData.pressEventCamera, out localCursor
@@ -56,19 +127,22 @@ public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         int xCoord = (int)(localCursor.x);
         int yCoord = (int)(localCursor.y);
 
-        Paint(xCoord, yCoord);
-
-        float m = (float)(yCoord - prevY) / (float)(xCoord - prevX);
-        float c = yCoord - m * xCoord;
-
-        if (xCoord < prevX) {
-            for (int x = xCoord; x < prevX; x++) {
-                Paint(x, (int)(m * x + c));
-            }
+        if (prevX == -1 || prevY == -1) {
+            Paint(xCoord, yCoord);
         } else {
-            for (int x = prevX; x < xCoord; x++) {
-                Paint(x, (int)(m * x + c));
+            float m = (float)(yCoord - prevY) / (float)(xCoord - prevX);
+            float c = yCoord - m * xCoord;
+            List<Vector2> coords = new List<Vector2>();
+            if (xCoord < prevX) {
+                for (int x = xCoord; x < prevX; x++) {
+                    coords.Add(new Vector2(x, (int)(m * x + c)));
+                }
+            } else {
+                for (int x = prevX; x < xCoord; x++) {
+                    coords.Add(new Vector2(x, (int)(m * x + c)));
+                }
             }
+            PaintGroup(coords);
         }
 
         prevX = xCoord;
@@ -76,8 +150,6 @@ public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
     }
 
     public void Paint(int x, int y) {
-        Debug.Log(x + ", " + y);
-
         if (x < 0 || x >= width || y < 0 || y >= height) {
             return;
         }
@@ -98,5 +170,27 @@ public class Painter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         texBuffer.Apply();
     }
 
-    public void OnEndDrag(PointerEventData eventData){}
+    public void PaintGroup(List<Vector2> coords) {
+        Color[] colours = texBuffer.GetPixels();
+        foreach (Vector2 coord in coords) {
+            if (coord.x < 0 || coord.x >= width || coord.y < 0 || coord.y >= height) {
+                continue;
+            }
+            for (int i = (int)coord.x - brushSize; i <= (int)coord.x + brushSize; i++) {
+                for (int j = (int)coord.y - brushSize; j <= (int)coord.y + brushSize; j++) {
+                    if (!(i < 0 || i >= width || j < 0 || j >= height)) {
+                        colours[j * height + i] = paintColour;
+                    }
+                }
+            }
+        }
+        texBuffer.SetPixels(colours);
+        image.material.mainTexture = texBuffer;
+        texBuffer.Apply();
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        prevX = -1;
+        prevY = -1;
+    }
 }
