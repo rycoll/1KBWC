7d9824ca6ed025f061c8afbce6c403275f36f503
Ryan <colry349@gmail.com>
2019-06-09 17:16:37 +1200

    Prepare for unit testing

---
+++ Assets/Tests/BytecodeStackTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NSubstitute; 
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BytecodeStackTest
+    {
+        private GameController game;
+
+        [Test]
+        public void TestIntBytecode([NUnit.Framework.Range(0, 1000, 100)] int num)
+        {
+            Interpreter interpreter = new Interpreter(game);
+            
+        }
+
+    }
+}
