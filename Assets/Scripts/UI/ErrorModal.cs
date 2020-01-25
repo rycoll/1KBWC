f1d7d3f9038ebc21dc3d915bd169d31dea5061c8
Ryan Collins <colry349@gmail.com>
2020-01-25 17:42:54 +1300

    New modal for reporting errors

---
+++ Assets/Scripts/UI/ErrorModal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorModal : MonoBehaviour
+{
+    [SerializeField]
+    private Text errorText;
+
+    public void ShowError(string errorMessage) {
+        this.gameObject.SetActive(true);
+        errorText.text = errorMessage;
+    }
+
+    public void Close() {
+        this.gameObject.SetActive(false);
+    }
+}
